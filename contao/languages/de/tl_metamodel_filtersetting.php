03253ec44ffe51fc98faf2d61a44a8090cd7e3a5
Mini Model <minimodel@metamodel.me>
2015-09-18 12:17:26 +0200

    Updated translations from transifex.

--- contao/languages/de/tl_metamodel_filtersetting.php
+++ contao/languages/de/tl_metamodel_filtersetting.php
@@ -8,10 +8,11 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2015-07-11T08:03:58+02:00
+ * last-updated: 2015-09-18T10:34:57+02:00
  */
 
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['hideempty']['0']        = 'Leere Einträge verbergen';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['hideempty']['1']        = 'Hier können Sie auswählen, ob leere Felder in der Liste angezeigt werden sollen oder nicht.';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlypossible']['0']     = 'Nur verbleibende Tags';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlypossible']['1']     = 'Zeige nur die Optionen mit Verknüpfungen nachdem der aktuelle Filter gesetzt ist.';
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['shownumbers']['0']      = 'Anzeige von Nummern';

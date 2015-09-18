17fdb4f204413373e09c0b586e85ba6cec5ccdf6
Mini Model <minimodel@metamodel.me>
2015-09-18 10:34:57 +0200

    Updated translations from transifex.

---
+++ contao/languages/fr/tl_metamodel_filtersetting.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/fr/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2015-07-11T08:03:58+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['hideempty']['0']    = 'Cacher les valeurs vides';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlypossible']['0'] = 'Étiquettes restantes seulement.';
+
---
+++ contao/languages/ru/tl_metamodel_filtersetting.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/ru/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2015-07-11T08:03:58+02:00
+ */
+
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['hideempty']['0']        = 'Скрыть пустые значения';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['hideempty']['1']        = 'Вы можете выбрать, должны скрываться пустые значения в списке или нет.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlypossible']['0']     = 'Только остальные теги';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['onlypossible']['1']     = 'Показывать только параметры, для которых есть результат после применения этого фильтра.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['shownumbers']['0']      = 'Показать числа';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['shownumbers']['1']      = 'Вы можете выбрать, должны показываться цифры или нет.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['register'] = 'Регистрация';
+
